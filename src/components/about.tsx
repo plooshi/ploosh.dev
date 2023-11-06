051f4e1aca062981c25af26fffedbe7a4281aaa3
Tom E <me@ploosh.dev>
2023-11-06 18:55:08 -0500

    Update about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -7,7 +7,7 @@ export default function About() {
                 <div className="title-box">
                     <Avatar src="https://avatars.githubusercontent.com/u/56302322" width="40px" />
                     <Spacer x={1} />
-                    <Text h2 id="title">Hey, I'm Ploosh!</Text>
+                    <Text h2 id="title">Hey, I'm water bottle!</Text>
                 </div>
                 <Spacer y={1} />
                 <Text h4 id="about_text">An independent developer from the USA. My name is Tom, and I go by he/they.
