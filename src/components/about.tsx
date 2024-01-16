0aba7f48c60d9785e4ae813fcd780a12c559bf7f
Tom E <me@ploosh.dev>
2024-01-15 19:33:00 -0500

    Update about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -10,8 +10,8 @@ export default function About() {
                     <Text h2 id="title">Hey, I'm Ploosh!</Text>
                 </div>
                 <Spacer y={1} />
-                <Text h4 id="about_text">An independent developer from the USA. My name is Tom, and I go by he/they.
-                I enjoy writing C, so most of my code is written in that. I have kind of a nerdy personality.</Text>
+                <Text h4 id="about_text">I am a developer from the USA. My name is Tom, and I go by he/him.
+                I enjoy writing C++, so most of my code is written in that.</Text>
             </div>
         </center>
     )
