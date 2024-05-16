70cb2903497cd88de835220b50a5049c6730efbe
ploosh <me@ploosh.dev>
2024-05-15 20:31:58 -0400

    shock

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -7,11 +7,10 @@ export default function About() {
                 <div className="title-box">
                     <Avatar src="https://avatars.githubusercontent.com/u/56302322" width="40px" />
                     <Spacer x={1} />
-                    <Text h2 id="title">Hey, I'm Ploosh!</Text>
+                    <Text h2 id="title">ploosh</Text>
                 </div>
                 <Spacer y={1} />
-                <Text h4 id="about_text">I am a developer from the USA. My name is Tom, and I go by he/him.
-                I enjoy writing C++, so most of my code is written in that.</Text>
+                <Text h4 id="about_text">C++ & x64 assembly dev</Text>
             </div>
         </center>
     )
